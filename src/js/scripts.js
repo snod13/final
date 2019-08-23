7ed5b51ad914892444af3d63615ad34f16f7d383
snod13 <a.50r0kin@yandex.ru>
2019-08-23 20:42:11 +0300

    Сверстан и адаптированы новости

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,4 +5,28 @@ $(document).ready(function() {
     $(this).toggleClass('menu__btn_active');
     $('.menu-burger').toggleClass('menu-burger_active');
   });
+  // слайдер
+  $('.slider').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    navContainer: '.announcement__nav',
+    navText: [
+      '<span class="announcement__btn announcement__left">Предыдущее</span><span class="announcement__line"></span>',
+      '<span class="announcement__btn announcement__right"> Следующее </span>'
+    ],
+    navElement: 'span',
+    navContainerClass: '.announcement__nav',
+    responsive: {
+      1200: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      0: {
+        items: 1
+      }
+    }
+  })
 });
